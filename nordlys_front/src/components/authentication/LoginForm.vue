6ac54960ecd89053ad51e4cf2178c20f973111b1
Lezenn <gireg.lezoraine@proton.me>
2025-08-09 23:55:38 +0200

    Advancements

--- nordlys_front/src/components/authentication/LoginForm.vue
+++ nordlys_front/src/components/authentication/LoginForm.vue
@@ -4,14 +4,14 @@
 
 <template>
 
-  <div class="card w-96 bg-base-100 card-lg shadow-sm">
+  <div class="card bg-base-100 card-lg shadow-sm">
     <div class="card-body">
       <h2 class="card-title">Connexion</h2>
       <form>
         <input type="text" placeholder="Adresse E-mail" class="input mb-2" />
         <input type="password" placeholder="Mot de passe" class="input" />
       </form>
-      <div class="justify-end card-actions">
+      <div class="justify-between card-actions">
         <RouterLink to="/" class="btn btn-primary">Se connecter</RouterLink>
         <RouterLink to="/" class="btn btn-secondary">Annuler</RouterLink>
       </div>
--- nordlys_front/src/pages/HomePage.vue
+++ nordlys_front/src/pages/HomePage.vue
@@ -1,9 +1,60 @@
 ﻿<script setup lang="ts">
 
+import LoginForm from "../components/authentication/LoginForm.vue";
 </script>
 
 <template>
-  <h1>Home page</h1>
+  <div class="flex flex-row gap-4 justify-between w-3/4 mx-auto">
+    <div class="flex flex-col gap-4">
+      <div class="card bg-base-100 shadow-sm">
+        <figure>
+          <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-base-100 shadow-sm">
+        <figure>
+          <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="max-w-1/4 flex flex-col gap-4">
+      <div class="card bg-base-100 shadow-sm">
+        <figure>
+          <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <LoginForm/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
